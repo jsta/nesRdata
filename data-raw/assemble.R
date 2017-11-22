92371ee080b67e917b50190b8ac818f72ac526ce
jsta <stachel2@msu.edu>
2017-11-22 16:02:43 -0500

    include raw csv

--- data-raw/assemble.R
+++ data-raw/assemble.R
@@ -4,3 +4,6 @@ nes_get("1")
 nes <- nes_load("1")$nes_data
 
 use_data(nes, overwrite = TRUE)
+
+dir.create(file.path("inst", "extdata"), showWarnings = FALSE)
+write.csv(nes, file.path("inst", "extdata", "nes.csv"), row.names = FALSE)
