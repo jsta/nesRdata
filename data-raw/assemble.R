6c05279a4f672f5d00b2421b6a55e47cf05c50f6
jsta <stachel2@msu.edu>
2019-12-10 09:03:54 -0500

    pull new data

--- data-raw/assemble.R
+++ data-raw/assemble.R
@@ -1,11 +1,11 @@
 # Pull top-level data from local source and store in package
-nes <- read.csv("../NES/nes_data.csv", stringsAsFactors = FALSE)
+nes <- nes_load(version_id = "5")$nes_data
 
 # Pull top-level data from dataone and store in package
 # nes_get("1")
 # nes <- nes_load("1")$nes_data
 
-devtools::use_data(nes, overwrite = TRUE)
+usethis::use_data(nes, overwrite = TRUE)
 
 dir.create(file.path("inst", "extdata"), showWarnings = FALSE)
 write.csv(nes, file.path("inst", "extdata", "nes.csv"), row.names = FALSE)
